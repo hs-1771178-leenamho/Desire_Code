e27546add0fe3349809d78b4247100b52e1e081c
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2023-11-03 13:09:39 +0900

    작성 참여한 스크립트 모음

---
+++ Scripts/SceneManager/TestSceneManage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestSceneManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            SceneManager.LoadScene("F3");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            SceneManager.LoadScene("F2");
+        }
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            SceneManager.LoadScene("F4");
+        }
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            SceneManager.LoadScene("Normal_Ending");
+        }
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            SceneManager.LoadScene("True_Ending");
+        }
+
+    }
+}
